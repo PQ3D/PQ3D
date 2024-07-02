b5a1dc0fadadf147bc3e536e7480bd3c747ca5d9
zhuziyu <zhuziyu.edward@gmail.com>
2024-07-02 13:45:44 +0800

    pq3d initial release

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# Coming soon!
+## <img src='https://pq3d.github.io/file/pq3d-logo.png' width=3%> Unifying 3D Vision-Language Understanding via Promptable Queries
+
+<p align="left">
+    <a href='https://arxiv.org/abs/2405.11442'>
+      <img src='https://img.shields.io/badge/Paper-PDF-red?style=plastic&logo=adobeacrobatreader&logoColor=red' alt='Paper PDF'>
+    </a>
+    <a href='https://arxiv.org/abs/2405.11442'>
+      <img src='https://img.shields.io/badge/Paper-arXiv-green?style=plastic&logo=arXiv&logoColor=green' alt='Paper arXiv'>
+    </a>
+    <a href='https://pq3d.github.io'>
+      <img src='https://img.shields.io/badge/Project-Page-blue?style=plastic&logo=Google%20chrome&logoColor=blue' alt='Project Page'>
+    </a>
+    <a href=''>
+      <img src='https://img.shields.io/badge/Model-Checkpoints-orange?style=plastic&logo=Google%20Drive&logoColor=orange' alt='Checkpoints(TODO)'>
+    </a>
+</p>
+
+[Ziyu Zhu](https://zhuziyu-edward.github.io/), 
+[Zhuofan Zhang](https://tongclass.ac.cn/author/zhuofan-zhang/), 
+[Xiaojian Ma](https://jeasinema.github.io/), 
+[Xuesong Niu](https://scholar.google.com/citations?user=iuPSV-0AAAAJ&hl=en),
+[Yixin Chen](https://yixchen.github.io/),
+[Baoxiong Jia](https://buzz-beater.github.io),
+[Zhidong Deng](https://www.cs.tsinghua.edu.cn/csen/info/1165/4052.htm)📧,
+[Siyuan Huang](https://siyuanhuang.com/)📧,
+[Qing Li](https://liqing-ustc.github.io/)📧
+
+This repository is the official implementation of the ECCV 2024 paper "Unifying 3D Vision-Language Understanding via Promptable Queries".
+
+[Paper](https://arxiv.org/pdf/2308.04352.pdf) |
+[arXiv](https://arxiv.org/abs/2308.04352) |
+[Project](https://3d-vista.github.io/) |
+[HuggingFace Demo](https://huggingface.co/spaces/SceneDiffuser/SceneDiffuserDemo) |
+[Checkpoints](https://drive.google.com/drive/folders/1UZ5V9VbPCU-ikiyj6NI4LyMssblwr1LC?usp=share_link)
+
+<div align=center>
+<img src='https://pq3d.github.io/file/teaser.png' width=100%>
+</div>
+
+### News
+
+- [ 2024.07 ] Release codes of model! TODO: Clean up training and evaluation
+
+### Abstract
+
+A unified model for 3D vision-language (3D-VL) understanding is expected to take various scene representations and perform a wide range of tasks in a 3D scene. However, a considerable gap exists between existing methods and such a unified model, due to the independent application of representation and insufficient exploration of 3D multi-task training. In this paper, we introduce PQ3D, a unified model capable of using Promptable Queries to tackle a wide range of 3D-VL tasks, from low-level instance segmentation to high-level reasoning and planning.Tested across ten diverse 3D-VL datasets, This is achieved through three key innovations: (1) unifying various 3D scene representations (i.e., voxels, point clouds, multi-view im- ages) into a shared 3D coordinate space by segment-level grouping, (2) an attention-based query decoder for task-specific information retrieval guided by prompts, and (3) universal output heads for different tasks to support multi-task training. PQ3D demonstrates impressive performance on these tasks, setting new records on most benchmarks. Particularly, PQ3D improves the state- of-the-art on ScanNet200 by 1.8% (AP), ScanRefer by 5.4% (acc@0.5), Multi3DRefer by 11.7% (F1@0.5), and Scan2Cap by 13.4% (CIDEr@0.5).Moreover, PQ3D supports flexible inference with individual or combined forms of available 3D representations, e.g., solely voxel input
+
+### Install
+1. Install conda package
+```
+conda env create --name envname --file=environments.yml
+```
+
+2. install pointnet2
+```
+TODO
+```
+
+3. Install Minkowski Engine
+```
+TODO
+```
+
+### Prepare data
+
+### Prepare checkpoints
+
+### Run PQ3D
+```
+TODO
+```
+
+### Acknowledgement
+We would like to thank the authors of [Vil3dref](https://github.com/cshizhe/vil3dref), [Mask3d](https://github.com/JonasSchult/Mask3D), [Openscene](https://github.com/pengsongyou/openscene), [Xdecoder](https://github.com/microsoft/X-Decoder), and [3D-VisTA](https://github.com/3d-vista/3D-VisTA) for their open-source release.
+
+
+### Citation:
+```
+@article{zhu2024unifying,
+    title={Unifying 3D Vision-Language Understanding via Promptable Queries},
+    author={Zhu, Ziyu and Zhang, Zhuofan and Ma, Xiaojian and Niu, Xuesong and Chen, Yixin and Jia, Baoxiong and Deng, Zhidong and Huang, Siyuan and Li, Qing},
+    journal={arXiv preprint arXiv:2405.11442},
+    year={2024}
+}
+```
---
+++ model/query3d_unified.py
@@ -0,0 +1,210 @@
+class Query3DUnified(BaseModel):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        # record parameters
+        self.cfg = cfg
+        self.memories = cfg.model.memories
+        self.heads = cfg.model.heads
+        self.use_offline_voxel_fts = cfg.model.get('use_offline_voxel_fts', False)
+        self.use_offline_attn_mask = cfg.model.get('use_offline_attn_mask', False)
+        self.inputs = self.memories[:]
+        self.pairwise_rel_type = self.cfg.model.obj_loc.pairwise_rel_type
+        self.spatial_dim = self.cfg.model.obj_loc.spatial_dim
+        self.num_heads = self.cfg.model.unified_encoder.args.num_attention_heads
+        self.skip_query_encoder_mask_pred = cfg.model.get('skip_query_encoder_mask_pred', False)
+        # build prompt type
+        self.prompt_types = ['txt', 'loc']
+        # build feature encoder
+        for input in self.inputs:
+            if input == 'prompt':
+                for prompt_type in self.prompt_types: # only text prompt for now
+                    if prompt_type == 'loc':
+                        continue
+                    encoder = prompt_type + '_encoder'
+                    setattr(self, encoder, build_module_by_name(cfg.model.get(encoder)))
+            else:
+                encoder = input + '_encoder'
+                setattr(self, encoder, build_module_by_name(cfg.model.get(encoder)))
+        # build location encoder
+        dim_loc = self.cfg.model.obj_loc.dim_loc
+        hidden_size = self.cfg.model.hidden_size
+        self.dim_loc = dim_loc
+        self.hidden_size = hidden_size
+        if self.dim_loc > 3:
+            self.coord_encoder = nn.Sequential(
+                nn.Linear(3, hidden_size),
+                nn.LayerNorm(hidden_size),
+            )
+            self.box_encoder = nn.Sequential(
+                nn.Linear(3, hidden_size),
+                nn.LayerNorm(hidden_size),
+            )
+        else:
+            self.coord_encoder = CoordinateEncoder(hidden_size)
+        # build unified encoder    
+        self.unified_encoder = build_module_by_name(self.cfg.model.unified_encoder)
+        # build task head
+        for head in self.heads:
+            head = head + '_head'
+            setattr(self, head, build_module_by_name(cfg.model.get(head)))
+        
+    def prompt_encoder(self, data_dict):
+        prompt = data_dict['prompt']
+        prompt_pad_masks = data_dict['prompt_pad_masks']
+        prompt_type = data_dict['prompt_type']
+        prompt_feat = torch.zeros(prompt.shape + (self.hidden_size,), device=prompt.device)
+        for type in self.prompt_types:
+            # get idx
+            idx = prompt_type == getattr(PromptType, type.upper())
+            if idx.sum() == 0:
+                continue
+            input = prompt[idx]
+            mask = prompt_pad_masks[idx]
+            # encode
+            if type == 'txt':
+                encoder = self.txt_encoder
+                feat = encoder(input.long(), mask)
+            elif type == 'loc':
+                loc_prompts = input[:, :self.dim_loc]
+                if self.dim_loc > 3:
+                    feat = self.coord_encoder(loc_prompts[:, :3]).unsqueeze(1) + self.box_encoder(loc_prompts[:, 3:6]).unsqueeze(1)
+                else:
+                    feat = self.coord_encoder(loc_prompts[:, :3].unsqueeze(1), input_range=[data_dict['coord_min'][idx], data_dict['coord_max'][idx]])
+                mask[:, 1:] = False
+            else:
+                raise NotImplementedError(f'{type} is not implemented')
+            # put back to orignal prompt
+            prompt_feat[idx] = feat
+            prompt_pad_masks[idx] = mask
+        return prompt_feat, prompt_pad_masks.logical_not()
+        
+    def forward(self, data_dict):
+        input_dict = {}
+        # build query
+        mask = data_dict['query_pad_masks'].logical_not()
+        query_locs = data_dict['query_locs'][:, :, :self.dim_loc]
+        coord_min = data_dict['coord_min']
+        coord_max = data_dict['coord_max']
+        if self.dim_loc > 3:
+            query_pos  = self.coord_encoder(query_locs[:, :, :3]) + self.box_encoder(query_locs[:, :, 3:6])
+        else:
+            query_pos = self.coord_encoder(query_locs[:, :, :3], input_range=[coord_min, coord_max])
+        feat = torch.zeros_like(query_pos)
+        pos = query_pos
+        input_dict['query'] = (feat, mask, pos)
+        # encode fts including point, voxel, image, and prompt
+        # the semantics of the attention mask in pytorch (True as masked) is the opposite as Huggingface Transformers (False as masked)  
+        fts_locs = data_dict['seg_center']
+        if self.dim_loc > 3:
+            fts_pos = self.coord_encoder(fts_locs[:, :, :3]) + self.box_encoder(fts_locs[:, :,  3:6])
+        else:
+            fts_pos = self.coord_encoder(fts_locs[:, :, :3], input_range=[coord_min, coord_max])
+        if self.dim_loc > 3:
+            fts_pos += self.box_encoder(fts_locs[:, :, 3:6])
+        for input in self.inputs:
+            feat, mask, pos = None, None, None
+            if input == 'prompt':
+                feat, mask = self.prompt_encoder(data_dict)
+            elif input == 'mv':
+                feat = self.mv_encoder(obj_feats = data_dict['mv_seg_fts'])
+                mask = data_dict['mv_seg_pad_masks'].logical_not()
+                pos = fts_pos
+            elif input == 'pc':
+                feat = self.pc_encoder(obj_feats = data_dict['pc_seg_fts'])
+                mask = data_dict['pc_seg_pad_masks'].logical_not()
+                pos = fts_pos
+            elif input == 'voxel':
+                if self.use_offline_voxel_fts:
+                    feat = self.voxel_encoder(data_dict['voxel_seg_fts'])
+                    mask = data_dict['voxel_seg_pad_masks'].logical_not()
+                else:
+                    voxel_features = data_dict['voxel_features']
+                    voxel_coordinates = data_dict['voxel_coordinates']
+                    x = ME.SparseTensor(coordinates=voxel_coordinates, features=voxel_features[:, :-3], device=voxel_features.device)
+                    voxel2segment = data_dict['voxel2segment']
+                    feat = self.voxel_encoder(x, voxel2segment, max_seg=fts_locs.shape[1])
+                    mask = data_dict['seg_pad_masks'].logical_not()
+                    data_dict['voxel_feat'] = {'feat': feat[-1].detach().cpu(), 'mask': mask.detach().cpu()}
+                pos = fts_pos
+            else:
+                raise NotImplementedError(f"Unknow input type: {input}")
+            input_dict[input] = [feat, mask, pos]
+        # build offline attention mask for guided mask training
+        if self.use_offline_attn_mask:
+            offline_attn_masks = data_dict['offline_attn_mask']
+        else:
+            offline_attn_masks = None
+        # generate features for mask head
+        seg_fts_for_match = []
+        for input in self.inputs:
+            if input in ['voxel', 'mv', 'pc']:
+                feats = copy(input_dict[input][:])
+                if isinstance(feats[0], list):
+                    assert input == 'voxel'
+                    feats[0] = feats[0][-1] # use the last scale of voxel features for segment matching
+                seg_fts_for_match.append(feats)                 
+        # build mask head
+        if hasattr(self, 'mask_head'):
+            mask_head_partial = partial(self.mask_head, seg_fts_for_match=seg_fts_for_match, seg_masks=data_dict['seg_pad_masks'].logical_not(),
+                                        offline_attn_masks=offline_attn_masks, skip_prediction=self.skip_query_encoder_mask_pred)
+        else:
+            mask_head_partial = None
+        # generate features for spatial attention
+        if self.unified_encoder.spatial_selfattn:
+            pairwise_locs = calc_pairwise_locs(query_locs[:, :, :3], None, 
+                                           pairwise_rel_type=self.pairwise_rel_type, spatial_dist_norm=True,
+                                           spatial_dim=self.spatial_dim)
+        else:
+            pairwise_locs = None
+            
+        # unified encoding                           
+        query, predictions_class, predictions_mask = self.unified_encoder(input_dict, pairwise_locs, mask_head_partial)
+        
+        # task head
+        for head in self.heads:
+            if head == 'ground':
+                inputs = [query, data_dict['query_pad_masks']]
+                label = data_dict["tgt_object_id"]
+                logits = getattr(self, head + '_head')(*inputs)
+                data_dict[head + '_logits'] = logits
+                data_dict['og3d_logits'] = logits
+                data_dict[head + '_label'] = label
+            elif head == 'generation':
+                label = data_dict["response"]
+                inputs = [query, data_dict['query_pad_masks']] + [label if self.training else None]
+                logits = getattr(self, head + '_head')(*inputs)
+                data_dict[head + '_logits'] = logits
+                data_dict[head + '_label'] = label
+            elif head == 'mask':
+                if self.skip_query_encoder_mask_pred:
+                    mask_head_partial = partial(self.mask_head, seg_fts_for_match=seg_fts_for_match, seg_masks=data_dict['seg_pad_masks'].logical_not(),
+                                    offline_attn_masks=offline_attn_masks, skip_prediction=False)
+                    predictions_class = []
+                    predictions_mask = []
+                pred_logits, pred_masks, _ = mask_head_partial(query=query)
+                predictions_class.append(pred_logits)
+                predictions_mask.append(pred_masks)
+                data_dict['predictions_class'] = predictions_class
+                data_dict['predictions_mask'] = predictions_mask
+                continue
+            else:
+                raise NotImplementedError(f"Unknow head type: {head}")
+       
+        return data_dict
+
+    def get_opt_params(self):
+        def get_lr(cfg, default_lr):
+            return default_lr if cfg is None or cfg.get("lr") is None else cfg.get("lr")
+
+        optimizer_grouped_parameters = []
+        for name, module in self._modules.items():
+            module_cfg = self.cfg.model.get(name)
+            lr = get_lr(module_cfg, self.cfg.solver.lr)
+            if lr != self.cfg.solver.lr:
+                print(f"Change lr from default {self.cfg.solver.lr} to {lr} for {name} module.")
+            optimizer_grouped_parameters += no_decay_param_group(module.named_parameters(), lr, name=name)
+
+        optimized_parameters = [p for group in optimizer_grouped_parameters for p in group['params']]
+        assert len(optimized_parameters) == len(list(self.parameters())), "Some parameters are not optimized!"
+        return optimizer_grouped_parameters
+        
